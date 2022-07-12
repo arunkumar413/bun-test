13179d1cb319985aeaec04d15a524a41a37c4289
Arun Kumar <arunkumar413@gmail.com>
2022-07-12 11:42:21 +0000

    save changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,11 @@ import logo from "./logo.svg";
 import * as React from "react";
 import "./App.css";
 
-function App() {
+export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Welcome to React!</h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Test</h1>
     </div>
   );
 }
 
-export default App;
